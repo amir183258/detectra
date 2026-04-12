f341513a006feb80857600164477b8ba95f7378b
amir183258 <amirhossein183258@gmail.com>
2026-04-12 23:38:30 +0330

    core: add ObjectDetector interface class to load and run detection models

---
+++ include/core/object_detector.hpp
@@ -0,0 +1,25 @@
+#ifndef CORE_OBJECT_DETECTOR_HPP
+#define CORE_OBJECT_DETECTOR_HPP
+
+#include <QString>
+#include <QImage>
+#include <QVector>
+
+#include "core/detector_box.hpp"
+
+class ObjectDetector {
+private:
+
+public:
+	/* virtual destructor is necessary */
+	virtual ~ObjectDetector() = default;
+
+	virtual bool load_model(const QString &model_path) = 0;
+
+	virtual QVector<DetectorBox> run(const QImage &image) = 0;
+
+	virtual bool is_ready() const = 0;
+
+};
+
+#endif
